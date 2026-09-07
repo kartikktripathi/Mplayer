91831749857e53322e0f994e98219edee00a2331
Kartik Tripathi <kartiktripathi103@gmail.com>
2026-09-07 22:31:04 +0530

    refactor: restore player.js functionality with song listing feature

--- player.js
+++ player.js
@@ -1,1 +1,21 @@
-console.log("Mplayer is under construction!");
+const fs = require("fs");
+const path = require("path");
+
+const songs = fs.readdirSync(path.join(__dirname, "Songs")).filter(file => !file.endsWith(".jpeg"));
+
+let selected = 1;
+showSongs();
+
+function showSongs() {
+    process.stdout.write('\x1b[2K')
+  for (let i = 0; i < songs.length; i++) {
+    // Clear line
+    if(selected === i+1){
+        console.log(`->${i + 1}: ${songs[i]}`);
+
+      }else{
+        console.log(`  ${i + 1}: ${songs[i]}`);
+      }
+
+  }
+}
