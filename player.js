8e6ff7cac92557806c73b65a610836771dfaa917
Kartik Tripathi <kartiktripathi103@gmail.com>
2026-09-07 22:35:12 +0530

    feat: add initial project files including package.json and .gitignore style: format player.js for improved readability

--- player.js
+++ player.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
-const songs = fs.readdirSync(path.join(__dirname, "Songs")).filter(file => !file.endsWith(".jpeg"));
+const songs = fs
+  .readdirSync(path.join(__dirname, "Songs"))
+  .filter((file) => !file.endsWith(".jpeg"));
 
 let selected = 1;
 showSongs();
 
 function showSongs() {
-    process.stdout.write('\x1b[2K')
+  process.stdout.write("\x1b[2K");
   for (let i = 0; i < songs.length; i++) {
     // Clear line
-    if(selected === i+1){
-        console.log(`->${i + 1}: ${songs[i]}`);
-
-      }else{
-        console.log(`  ${i + 1}: ${songs[i]}`);
-      }
-
+    if (selected === i + 1) {
+      console.log(`->${i + 1}: ${songs[i]}`);
+    } else {
+      console.log(`  ${i + 1}: ${songs[i]}`);
+    }
   }
 }
