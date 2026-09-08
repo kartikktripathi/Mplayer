810b00cc4eb5eee5ab8c3c66926731662287d585
Kartik Tripathi <kartiktripathi103@gmail.com>
2026-09-08 09:32:18 +0530

    chore: add ESLint and related packages to devDependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    // This tells ESLint to apply these rules to JavaScript files only.
+    files: ["**/*.js"],
+    
+    // Here, we are adding Node.js global variables.
+    languageOptions: {
+      globals: {
+        ...globals.node, // This enables "process", "__dirname", "require", etc.
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
